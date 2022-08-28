681c012d10cbb7b791d381f3a21cfe027c3039af
zqq90 <zqq_90@163.com>
2022-08-28 16:47:46 +0800

    chore: license headers

--- jooq-codegen/meta/src/main/java/org/febit/jooq/codegen/meta/MetaUtils.java
+++ jooq-codegen/meta/src/main/java/org/febit/jooq/codegen/meta/MetaUtils.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2022-present febit.org (support@febit.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.febit.jooq.codegen.meta;
 
 import lombok.experimental.UtilityClass;
@@ -12,38 +27,60 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.UncheckedIOException;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 @UtilityClass
 public class MetaUtils {
 
     public static final String JOOQ_PKG = "org.febit.jooq";
     public static final String CODEGEN_PKG = JOOQ_PKG + ".codegen";
-
+    public static final String CLASS_GENERATOR = CODEGEN_PKG + ".JooqJavaGenerator";
+    public static final String CLASS_STRATEGY = CODEGEN_PKG + ".JooqGeneratorStrategy";
     public static final String CODEGEN_JOOQ = "codegenJooq";
     public static final String GROUP_NAME = "Codegen jOOQ";
     public static final String CODEGEN_JOOQ_FOLDER = "codegen-jooq";
-
-    public static final String CLASS_GENERATOR = CODEGEN_PKG + ".JooqJavaGenerator";
-    public static final String CLASS_STRATEGY = CODEGEN_PKG + ".JooqGeneratorStrategy";
     public static final String CLASS_MAIN = "org.jooq.codegen.GenerationTool";
 
-    public Generator createGenerator() {
-        return new Generator()
-                .withName(CLASS_GENERATOR)
-                .withDatabase(new Database())
-                .withTarget(new Target())
-                .withStrategy(new Strategy()
-                        .withName(CLASS_STRATEGY)
-                )
-                .withGenerate(new Generate()
-                        .withPojos(true)
-                        .withFluentSetters(false)
-                        .withImmutablePojos(false)
-                        .withJavaTimeTypes(true)
-                        .withJavaBeansGettersAndSetters(true)
-                        .withJpaAnnotations(true)
-                        .withValidationAnnotations(true)
-                );
+    public static Generator createAndInitGenerator() {
+        var g = new Generator();
+        initGenerator(g);
+        return g;
+    }
+
+    public static void initGenerator(Generator g) {
+        g.setName(CLASS_GENERATOR);
+
+        computeIfAbsent(g::getDatabase, g::setDatabase, Database::new);
+        computeIfAbsent(g::getTarget, g::setTarget, Target::new);
+        computeIfAbsent(g::getStrategy, g::setStrategy, Strategy::new);
+        computeIfAbsent(g::getGenerate, g::setGenerate, Generate::new);
+
+        var strategy = g.getStrategy();
+        strategy.withName(CLASS_STRATEGY);
+
+        initGenerate(g.getGenerate());
+    }
+
+    private static <P> void computeIfAbsent(Supplier<P> getter, Consumer<P> setter, Supplier<P> supplier) {
+        if (getter.get() == null) {
+            setter.accept(supplier.get());
+        }
+    }
+
+    public static void initGenerate(Generate generate) {
+        generate.withPojos(true)
+                .withJavaTimeTypes(true)
+                .withFluentSetters(false)
+                .withJavaBeansGettersAndSetters(true)
+                .withJpaAnnotations(true)
+                .withValidationAnnotations(true);
+    }
+
+    private static <P> void setIfAbsent(Supplier<P> getter, Consumer<P> setter, P defaultValue) {
+        if (getter.get() == null) {
+            setter.accept(defaultValue);
+        }
     }
 
     public static void emitConfigSilent(Configuration conf, File file) {
--- jooq-codegen/runtime/src/main/java/org/febit/jooq/codegen/SpiUtils.java
+++ jooq-codegen/runtime/src/main/java/org/febit/jooq/codegen/SpiUtils.java
@@ -1,10 +1,22 @@
+/*
+ * Copyright 2022-present febit.org (support@febit.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.febit.jooq.codegen;
 
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
-import lombok.val;
 import org.febit.util.Lists;
 import org.springframework.core.annotation.AnnotationAwareOrderComparator;
 
