e7dd8857d2af6098eb39fb1adcdf25962f18d91e
zqq90 <zqq_90@163.com>
2022-08-28 18:37:31 +0800

    feat(codegen): emit names of tables & columns

--- jooq-codegen/runtime/src/main/java/org/febit/jooq/codegen/JooqJavaGenerator.java
+++ jooq-codegen/runtime/src/main/java/org/febit/jooq/codegen/JooqJavaGenerator.java
@@ -17,11 +17,13 @@ package org.febit.jooq.codegen;
 
 import org.febit.jooq.codegen.meta.MetaUtils;
 import org.febit.jooq.codegen.spi.DatabaseFilter;
+import org.febit.util.JacksonUtils;
 import org.jooq.codegen.FebitDevkitJavaGeneratorHack;
 import org.jooq.codegen.GeneratorStrategy;
 import org.jooq.codegen.JavaWriter;
 import org.jooq.meta.ColumnDefinition;
 import org.jooq.meta.Database;
+import org.jooq.meta.SchemaDefinition;
 import org.jooq.meta.TableDefinition;
 
 import java.util.List;
@@ -44,6 +46,12 @@ public class JooqJavaGenerator extends FebitDevkitJavaGeneratorHack {
     }
 
     @Override
+    protected void generateTableReferencesClassFooter(SchemaDefinition schema, JavaWriter out) {
+        emitTableReferencesNamesClass(schema, out);
+        super.generateTableReferencesClassFooter(schema, out);
+    }
+
+    @Override
     protected void generatePojoClassFooter(TableDefinition table, JavaWriter out) {
         emitPojoIdMethod(table, out);
         emitPojoToRecordMethod(table, out);
@@ -54,6 +62,7 @@ public class JooqJavaGenerator extends FebitDevkitJavaGeneratorHack {
     protected void generateTableClassFooter(TableDefinition table, JavaWriter out) {
         emitTableGetIdFieldMethod(table, out);
         emitTablePkExcludedFieldsMethod(table, out);
+        emitTableColumnsClass(table, out);
         super.generateTableClassFooter(table, out);
     }
 
@@ -72,9 +81,9 @@ public class JooqJavaGenerator extends FebitDevkitJavaGeneratorHack {
         var idColumnId = getStrategy().getJavaIdentifier(idColumn);
 
         out.println();
-        out.tab(1).println("public Field<%s> pkField() {", idType);
-        out.tab(2).println("return %s;", idColumnId);
-        out.tab(1).println("}");
+        out.println("public Field<%s> pkField() {", idType);
+        out.println("return %s;", idColumnId);
+        out.println("}");
     }
 
     /**
@@ -82,19 +91,41 @@ public class JooqJavaGenerator extends FebitDevkitJavaGeneratorHack {
      */
     private void emitTablePkExcludedFieldsMethod(TableDefinition table, JavaWriter out) {
         var recordClassName = getStrategy().getJavaClassName(table, GeneratorStrategy.Mode.RECORD);
+
+        var columns = table.getColumns().stream()
+                .filter(col -> !col.isIdentity())
+                .collect(Collectors.toList());
+
         out.println();
         out.ref(Collectors.class);
         out.ref(List.class);
-        out.tab(1).println("private List<TableField<%s, ?>> __FIELDS_EXCLUDED_PKS;", recordClassName);
+
+        out.println("private final List<TableField<%s, ?>> pkExcludedFields = List.of(", recordClassName);
+        var lastIdx = columns.size() - 1;
+        for (int i = 0; i < columns.size(); i++) {
+            var columnId = getStrategy().getJavaIdentifier(columns.get(i));
+            out.println(i == lastIdx ? "%s" : "%s,", columnId);
+        }
+        out.println(");");
+
         out.println();
-        out.tab(1).println("public List<TableField<%s, ?>> pkExcludedFields() {", recordClassName);
-        out.tab(2).println("List<TableField<%s, ?>> fields = __FIELDS_EXCLUDED_PKS;", recordClassName);
-        out.tab(2).println("return fields != null ? fields");
-        out.tab(4).println(": (__FIELDS_EXCLUDED_PKS = fieldStream()");
-        out.tab(4).println(".map(f -> (TableField<%s, ?>) f)", recordClassName);
-        out.tab(4).println(".filter(f -> !f.getDataType().identity())");
-        out.tab(4).println(".collect(Collectors.toList()));");
-        out.tab(1).println("}");
+        out.println("public List<TableField<%s, ?>> pkExcludedFields() {", recordClassName);
+        out.println("return pkExcludedFields;");
+        out.println("}");
+    }
+
+    /**
+     * Emit Table.Columns class.
+     */
+    private void emitTableColumnsClass(TableDefinition table, JavaWriter out) {
+        out.println();
+        out.println("public static class Columns {");
+        for (var column : table.getColumns()) {
+            var columnId = getStrategy().getJavaIdentifier(column);
+            var columnName = column.getName();
+            out.println("public static final String %s = %s;", columnId, JacksonUtils.toJsonString(columnName));
+        }
+        out.println("}");
     }
 
     /**
@@ -138,6 +169,24 @@ public class JooqJavaGenerator extends FebitDevkitJavaGeneratorHack {
         out.tab(1).println("}");
     }
 
+    private String resolveColumnFullType(ColumnDefinition column, JavaWriter out) {
+        var typeDef = column.getType(resolver(out, GeneratorStrategy.Mode.POJO));
+        return getJavaType(typeDef, out, GeneratorStrategy.Mode.POJO);
+    }
+
+    /**
+     * Emit Tables.Names class.
+     */
+    private void emitTableReferencesNamesClass(SchemaDefinition schema, JavaWriter out) {
+        out.println();
+        out.println("public static class Names {");
+        for (var table : schema.getTables()) {
+            var id = getStrategy().getJavaIdentifier(table);
+            var name = table.getName();
+            out.println("public static final String %s = %s;", id, JacksonUtils.toJsonString(name));
+        }
+        out.println("}");
+    }
 
     /**
      * Emit Record#fromPojo(..) method.
@@ -154,10 +203,5 @@ public class JooqJavaGenerator extends FebitDevkitJavaGeneratorHack {
         out.tab(2).println("record.from(po);");
         out.tab(2).println("return record;");
         out.tab(1).println("}");
-    }
-
-    private String resolveColumnFullType(ColumnDefinition column, JavaWriter out) {
-        var typeDef = column.getType(resolver(out, GeneratorStrategy.Mode.POJO));
-        return getJavaType(typeDef, out, GeneratorStrategy.Mode.POJO);
     }
 }
