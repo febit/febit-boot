2dcdb31b14645e665475393e3aa62d63a086a0a9
zqq90 <zqq_90@163.com>
2023-08-31 21:53:52 +0800

    refactor(jwt): mv package

--- boot-common/src/main/java/org/febit/boot/jwt/JwtCodecProps.java
+++ boot-common/src/main/java/org/febit/boot/jwt/JwtCodecProps.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.febit.boot.common.jwt;
+package org.febit.boot.jwt;
 
 import com.nimbusds.jose.JWSSigner;
 import jakarta.annotation.Nullable;
